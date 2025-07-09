65214c3727d0878712cbd24c012fda297de0ab22
kakakadudu <44600723+kakakadudu@users.noreply.github.com>
2025-07-09 15:30:50 +0800

    Add files via upload

--- src/views/Chrome.vue
+++ src/views/Chrome.vue
@@ -41,6 +41,23 @@
       </div>
     </div>
     <div class="chrome-content">
+      <div class="nav-top p10 f-c-e">
+        <a href="https://github.com/kakakadudu" target="_blank">
+          <svg
+            height="24"
+            width="24"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            version="1.1"
+            data-view-component="true"
+            class="github"
+          >
+            <path
+              d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"
+            ></path>
+          </svg>
+        </a>
+      </div>
       <div class="el-logo h168 mb38 f-e-c">
         <div class="logo w272 h92"></div>
       </div>
@@ -65,7 +82,7 @@
 
       <!-- 常用地址 -->
       <div class="menu-box f-c-c">
-        <div class="menu-list f-c-c">
+        <div class="menu-list f-c-c flex-wrap">
           <div
             v-for="(item, idx) in menus.filter((f) => !f.hide)"
             :key="idx"
@@ -109,10 +126,12 @@ import {
   Files,
   Setting,
   Plus,
+  Brush,
 } from "@element-plus/icons-vue";
 import TimePeroidPicker from "@/views/TimePeroidPicker.vue";
 import Cascader from "@/views/Cascader.vue";
 import LoadMore from "@/views/LoadMore.vue";
+import Parallax from "@/views/Parallax.vue";
 import type { Component } from "vue";
 import type { TabType, MenuType } from "@/types/Chrome";
 import Action from "@/components/Action.vue";
@@ -146,6 +165,12 @@ const menus = ref<MenuType[]>([
     icon: markRaw(Files),
     component: markRaw(LoadMore),
     path: "LoadMore",
+  },
+  {
+    title: "Parallax",
+    icon: markRaw(Brush),
+    component: markRaw(Parallax),
+    path: "Parallax",
   },
   {
     title: "添加快捷方式",
@@ -228,6 +253,16 @@ const handleClickTab = (idx: number) => {
     width: 100%;
     height: calc(100% - 45px);
     overflow: hidden;
+
+    .nav-top {
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 100%;
+      .github {
+        fill: #fff;
+      }
+    }
   }
   .component-content {
     position: absolute;
@@ -387,6 +422,12 @@ const handleClickTab = (idx: number) => {
   .item-title {
     display: block;
     max-width: 100%;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .chrome {
+    --content-width: 90vw;
   }
 }
 </style>
---
+++ src/views/Parallax.vue
@@ -0,0 +1,92 @@
+<template>
+  <div ref="container" class="parallax">
+    <div
+      v-for="(section, idx) in sections"
+      :key="idx"
+      :style="{ backgroundImage: `url(${section.image})` }"
+      class="section-item f-c-c"
+    >
+      <!-- <div class="br10" v-if="idx === 0">
+        <div class="nav-top pl5 pr5 h40 br10 f-c-c">
+          <div class="nav-item f-c-c w80 white h30 br8 active">1</div>
+          <div class="nav-item f-c-c w80 white h30 br8">2</div>
+          <div class="nav-item f-c-c w80 white h30 br8">3</div>
+        </div>
+      </div>  -->
+      <div class="f-c-c white fs30 fw600">{{ section.text }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="Parallax">
+import { ref, onMounted, onUnmounted } from "vue";
+
+const sections = ref<
+  {
+    text: string;
+    image: string;
+  }[]
+>([
+  {
+    text: "THIS IS THE FIRST",
+    image:
+      "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?q=80&w=2070&auto=format&fit=crop",
+  },
+  {
+    text: "THIS IS THE SECOND",
+    image: "https://picsum.photos/1600/800?random=2",
+  },
+  {
+    text: "THIS IS THE THIRD",
+    image: "https://picsum.photos/1600/800?random=3",
+  },
+  {
+    text: "THIS IS THE FOURTH",
+    image: "https://picsum.photos/1600/800?random=4",
+  },
+  {
+    text: "THIS IS THE FIFTH",
+    image: "https://picsum.photos/1600/800?random=5",
+  },
+  {
+    text: "THIS IS THE SIXTH",
+    image: "https://picsum.photos/1600/800?random=6",
+  },
+]);
+</script>
+
+<style lang="scss" scoped>
+.parallax {
+  height: 100%;
+  overflow-x: hidden;
+
+  .section-item {
+    position: relative;
+    height: 100%;
+    width: 100vw;
+    background: center center / cover no-repeat fixed;
+    text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.8);
+  }
+
+  .nav-top {
+    backdrop-filter: blur(6px);
+    outline: 1px solid rgba(255, 255, 255, 0.2);
+    background-color: rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 0px 1px rgba(255, 255, 255, 0.5);
+
+    .nav-item.active {
+      background-color: rgba(73, 132, 73, 0.9);
+    }
+  }
+
+  .section-blur {
+    position: relative;
+    width: 200px;
+    height: 200px;
+    backdrop-filter: blur(6px);
+    outline: 1px solid rgba(255, 255, 255, 0.2);
+    background-color: rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 0px 1px rgba(255, 255, 255, 0.5);
+  }
+}
+</style>
